7eafd44e213995bb5b231be214fa3b38418888f1
EmilianoJZX100 <5083@holbertonschool.com>
2022-08-28 10:10:49 -0700

    selection sort finish

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,23 +1,40 @@
 #include "sort.h"
 
 /**
+ * swap - swaps positions of the arrays
+ * @a: pointer
+ * @b: pointer
+ */
+void swap(int *a, int *b)
+{
+	int c = 0;
+
+	c = *a;
+	*a = *b;
+	*b = c;
+}
+
+/**
  * selection_sort - sorts array of nums in ascending order using Selection sort
  * @array: numbers to sort
  * @size: size of the array of numbers
  */
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j;
-
-	if (!array)
-		return;
+	size_t i, j, index;
 
 	for (i = 0 ; i < size - 1; i++)
 	{
-		for (j = i ; j < size - 1; j++)
+		index = i;
+		for (j = i + 1; j < size; j++)
 		{
-			if (array[i] > array[j])
-				array[i] = array[j];
+			if (array[j] < array[index])
+				index = j;
+		}
+		if (index != i)
+		{
+			swap(&array[index], &array[i]);
+			print_array(array, size);
 		}
 	}
 }
