2b51d2e823d4b320ed67eb64a94c804801924458
EmilianoJZX100 <5083@holbertonschool.com>
2022-08-28 09:21:26 -0700

    insertion sort list fix

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -17,25 +17,25 @@ void insertion_sort_list(listint_t **list)
 		while (head->next != NULL)
 		{
 			if (head->n > head->next->n)
-				{
-					temp = head;
+			{
+				temp = head;
 
-					if (head->prev != NULL)
-						head->prev->next = temp->next;
-					head->next->prev = temp->prev;
-					head->prev = temp->next;
-					head->next = temp->next->next;
-					head->prev->next = temp;
+				if (head->prev != NULL)
+					head->prev->next = temp->next;
+				head->next->prev = temp->prev;
+				head->prev = temp->next;
+				head->next = temp->next->next;
+				head->prev->next = temp;
 
-					if (head->next != NULL)
-						head->next->prev = temp;
-					if (head->prev->prev == NULL)
-						*list = head->prev;
-					print_list(*list);
-					head = *list;
-					break;
-				}
-				head = head->next;
+				if (head->next != NULL)
+					head->next->prev = temp;
+				if (head->prev->prev == NULL)
+					*list = head->prev;
+				print_list(*list);
+				head = *list;
+				break;
+			}
+			head = head->next;
 		}
 	}
 }
